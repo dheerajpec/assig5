64f6f9d57522e1736ffef4697d8683a16677843d
dheerajpec <98840082+dheerajpec@users.noreply.github.com>
2022-06-15 21:08:04 +0530

    Update q3.py

--- q3.py
+++ q3.py
@@ -1,6 +1,8 @@
 a='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 n= int(input("enter the number: "))
 for i in range(1,n+1):
+    if len(a)<n:
+        a=a+a1
     for j in range(0,i):
         print(a[j], end='')
     print()
