0d5e00912701013d6ae0cd0ba1f962f623805542
dheerajpec <98840082+dheerajpec@users.noreply.github.com>
2022-06-15 23:32:52 +0530

    Update secondlast.py

--- secondlast.py
+++ secondlast.py
@@ -1,8 +1,8 @@
-a=[0,0,0,0,0,0,0,0,0,0]
-i=0
-while i<10:
-    a[i]= int(input("Enter the number: "))
-    i=i+1
+a=[0,0,0,0,0,0,0,0,0,0]# initially taken zero for declaration purpose to fill the values afterward
+k=0
+while k<10:
+    a[k]= int(input("Enter the number: "))
+    k=k+1
 print('The positive number out of the entered nos. are: ')
 for i in a:
     if(i>0):
@@ -17,7 +17,7 @@ for i in a:
         print(i)
 print('odd no out of the entered nos: ')
 for i in a:
-    if(not(i%2==0)):
+    if(not(i%2==0)):# not because if the condition comes false means it is odd and we need to print it so to get into the if statement not used
         print(i)
 print('Frequecy of respective nos are: ')
 for i in a:
@@ -28,7 +28,7 @@ for i in a:
     while j<10:
         if(a[j]==i):
             n=n+1
-            a[j]=1.1# an decimal no which would be used afterwards to distinguish it from others
+            a[j]=1.1# an decimal no which would be used afterwards to distinguish it from other int values
         j=j+1
     print('Frequency of ',i,'is :',n)
             
