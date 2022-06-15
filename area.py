e89561a392112e8c75f4e217b4c79d55b9baa370
dheerajpec <98840082+dheerajpec@users.noreply.github.com>
2022-06-15 23:19:00 +0530

    Update area.py

--- area.py
+++ area.py
@@ -1,7 +1,11 @@
 import math
-a=int(input("ENTER THE FIRST SIDE: "))
-b=int(input("ENTER THE second SIDE: "))
-c=int(input("ENTER THE third SIDE: "))
-s=(a+b+c)/2
-area= math.sqrt(s*(s-a)*(s-b)*(s-c))
-print('Area of the triangle: ',area)
+a=int(input("ENTER THE FIRST SIDE: "))# first side
+b=int(input("ENTER THE second SIDE: "))# second side
+c=int(input("ENTER THE third SIDE: "))# third side
+if(a+b>c and b+c>a and c+a>b):
+  s=(a+b+c)/2
+  area= math.sqrt(s*(s-a)*(s-b)*(s-c))
+  print('Area of the triangle: ',area)
+else: 
+  print('Triangle not possible with given dimentions')
+  
