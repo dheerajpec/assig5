08dcf06ab248e57cf8b66a11503f562f0c001abf
dheerajpec <98840082+dheerajpec@users.noreply.github.com>
2022-06-15 23:21:18 +0530

    Update q3.py

--- q3.py
+++ q3.py
@@ -3,7 +3,7 @@ a=a1
 n= int(input("enter the number: "))
 for i in range(1,n+1):
     if len(a)<n:
-        a=a+a1
+        a=a+a1# in case the string exhuasts so it will again start from A and so on
     for j in range(0,i):
         print(a[j], end='')
     print()
