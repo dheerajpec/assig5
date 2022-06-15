77db57c22cc285be14486c85d3c04d35301f5b86
dheerajpec <98840082+dheerajpec@users.noreply.github.com>
2022-06-16 00:21:00 +0530

    Update string.py

--- string.py
+++ string.py
@@ -2,13 +2,13 @@ a= ['dheeraj','dheeraj','vaibhav','gursimar','amisha','vaibhav','kundu','kundu']
 for i in a:
     g=0
     n=0
-    if(i=='----'):
+    if(type(i)==int):# this will distinguish it from others
         continue
 
     for j in a:
         if(i==j):
             n=n+1
-            a[g]='----'# just to make it different from others for differentiating purpose
+            a[g]=0 # just to make it different from others for differentiating purpose
         g=g+1
     print(i," occured ",n," times")
     
