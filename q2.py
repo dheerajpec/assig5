8bb806bb3b443f6f83c9608b8739a5422e72b87c
dheerajpec <98840082+dheerajpec@users.noreply.github.com>
2022-06-15 23:22:31 +0530

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,8 +1,8 @@
-for i in range(1,6):
+for i in range(1,6):# this will print the first half
     for j in range(0,i):
         print('*', end='')
-    print()
-for i in range(0,5):
+    print()# this will change the line
+for i in range(0,5):# this will print the second half
     j=4-i
     while(j>0):
         print('*', end='')
